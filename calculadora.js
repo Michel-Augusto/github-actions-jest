0520b7425927766b74323eeafd06dc83ae4c880c
Michel Augusto <michelaugusto80@gmail.com>
2024-02-22 10:06:41 -0300

    Its work?

--- calculadora.js
+++ calculadora.js
@@ -8,11 +8,16 @@ function subtracao(a, b) {
 } ;
 
 function multiplicacao(a, b) {
-    return a - b
+    return a * b
+} ;
+
+function divisao(a, b) {
+    return a / b
 } ;
 
 module.exports = {
     soma: soma,
     subtracao: subtracao,
-    multiplicacao: multiplicacao
+    multiplicacao: multiplicacao,
+    divisao: divisao
 } ;
