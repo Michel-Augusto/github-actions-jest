c60dcd3d090393e13c4ef917ce563b49a2075e32
Michel Augusto <michelaugusto80@gmail.com>
2024-02-22 09:57:13 -0300

    adding new function to test coverage validation

--- calculadora.js
+++ calculadora.js
@@ -1,13 +1,18 @@
 
 function soma(a, b) {
     return a + b
-}
+} ;
 
 function subtracao(a, b) {
     return a - b
-}
+} ;
+
+function multiplicacao(a, b) {
+    return a - b
+} ;
 
 module.exports = {
     soma: soma,
-    subtracao: subtracao
+    subtracao: subtracao,
+    multiplicacao: multiplicacao
 } ;
